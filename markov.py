52a7c6854e55697d9cac1b1caa08da3977eafa3e
Joshua T Corbin <jcorbin@wunjo.org>
2009-07-19 16:06:55 -0400

    Convert to classmethods

--- markov.py
+++ markov.py
@@ -1,72 +1,92 @@
 import re
 
-def sentences(iterable, endPunc=r"\.;"):
-    "Iterates sentences in a source"
-    if not endPunc in sentences._reCache:
-        sentences._reCache[endPunc] = re.compile(r"(.+)["+endPunc+r"]\s*(.*)")
-    sen = sentences._reCache[endPunc]
+class Parse(object):
+    _sentenceReCache = {}
 
-    buf = []
-    for line in iterable:
-        line = line.strip()
-        while len(line):
-            m = sen.match(line)
-            if m:
-                (frag, rest) = m.groups()
-                if len(buf):
-                    buf.append(frag)
-                    yield ' '.join(buf)
-                    buf = []
+    @classmethod
+    def sentences(cls, iterable, endPunc=r"\.;"):
+        """
+        Iterates sentences in a source
+        """
+        if not endPunc in cls._sentenceReCache:
+            cls._sentenceReCache[endPunc] = re.compile(
+                r"(.+)["+endPunc+r"]\s*(.*)"
+            )
+        sen = cls._sentenceReCache[endPunc]
+
+        buf = []
+        for line in iterable:
+            line = line.strip()
+            while len(line):
+                m = sen.match(line)
+                if m:
+                    (frag, rest) = m.groups()
+                    if len(buf):
+                        buf.append(frag)
+                        yield ' '.join(buf)
+                        buf = []
+                    else:
+                        yield frag
+                    line = rest
                 else:
-                    yield frag
-                line = rest
-            else:
-                buf.append(line)
-                line = ''
+                    buf.append(line)
+                    line = ''
 
-sentences._reCache = {}
+    _phraseReCache = {}
 
-def phrases(sentence, wordChar=r"[\w\-']", size=3, trailing=(None, None)):
-    """
-    Iterates phrases in a sentence
+    @classmethod
+    def phrases(
+        cls,
+        sentence,
+        wordChar=r"[\w\-']",
+        size=3,
+        trailing=(None, None)
+    ):
+        """
+        Iterates phrases in a sentence
 
-    Arguments
-        wordChar: the character class to use in finding words
-        size: how many words long should each phrase be
-        trailing: how many words to yield with None padding elements at
-            either end of the sentence; for example:
-                >>> s = 'alpha bravo charlie delta'
-                >>> pprint(list(phrases(s, size=3, trailing=(1, 1))))
-                [(None, None, 'alpha'),
-                 (None, 'alpha', 'bravo'),
-                 ('alpha', 'bravo', 'charlie'),
-                 ('bravo', 'charlie', 'delta'),
-                 ('charlie', 'delta', None),
-                 ('delta', None, None)]
-                >>> pprint(list(phrases(s, size=3, trailing=(None, 2))))
-                [('alpha', 'bravo', 'charlie'),
-                 ('bravo', 'charlie', 'delta'),
-                 ('charlie', 'delta', None)]
-    """
-    if not wordChar in phrases._reCache:
-        phrases._reCache[wordChar] = re.compile(r"\b("+wordChar+r"+)\b")
-    wordRe = phrases._reCache[wordChar]
+        Arguments
+            wordChar: the character class to use in finding words
+            size: how many words long should each phrase be
+            trailing: how many words to yield with None padding elements
+            at either end of the sentence; for example:
+                    >>> s = 'alpha bravo charlie delta'
+                    >>> pprint(list(
+                    ...     phrases(s, size=3, trailing=(1, 1))
+                    ... ))
+                    [(None, None, 'alpha'),
+                     (None, 'alpha', 'bravo'),
+                     ('alpha', 'bravo', 'charlie'),
+                     ('bravo', 'charlie', 'delta'),
+                     ('charlie', 'delta', None),
+                     ('delta', None, None)]
+                    >>> pprint(list(
+                    ...     phrases(s, size=3, trailing=(None, 2))
+                    ... ))
+                    [('alpha', 'bravo', 'charlie'),
+                     ('bravo', 'charlie', 'delta'),
+                     ('charlie', 'delta', None)]
+        """
+        if not wordChar in cls._phraseReCache:
+            cls._phraseReCache[wordChar] = re.compile(
+                r"\b("+wordChar+r"+)\b"
+            )
+        wordRe = cls._phraseReCache[wordChar]
 
-    buf = [] if trailing[0] is None else [None] * (size-trailing[0])
+        buf = [] if trailing[0] is None else [None] * (size-trailing[0])
 
-    for word in wordRe.finditer(sentence):
-        buf.append(word.group(0))
-        if len(buf) > size:
-            buf.pop(0)
-        if len(buf) == size:
-            yield tuple(buf)
+        for word in wordRe.finditer(sentence):
+            buf.append(word.group(0))
+            if len(buf) > size:
+                buf.pop(0)
+            if len(buf) == size:
+                yield tuple(buf)
 
-    if trailing[1] is not None:
-        for i in xrange(size-trailing[1]):
-            buf.append(None)
-            buf.pop(0)
-            yield tuple(buf)
+        if trailing[1] is not None:
+            for i in xrange(size-trailing[1]):
+                buf.append(None)
+                buf.pop(0)
+                yield tuple(buf)
 
-phrases._reCache = {}
 
 # vim:set ts=4 sw=4 expandtab:
