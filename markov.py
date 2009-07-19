810897470c97e6f381d9a436751d83e590cd337a
Joshua T Corbin <jcorbin@wunjo.org>
2009-07-19 13:34:54 -0400

    Add phrases generator

--- markov.py
+++ markov.py
@@ -26,4 +26,47 @@ def sentences(iterable, endPunc=r"\.;"):
 
 sentences._reCache = {}
 
+def phrases(sentence, wordChar=r"[\w\-']", size=3, trailing=(None, None)):
+    """
+    Iterates phrases in a sentence
+
+    Arguments
+        wordChar: the character class to use in finding words
+        size: how many words long should each phrase be
+        trailing: how many words to yield with None padding elements at
+            either end of the sentence; for example:
+                >>> s = 'alpha bravo charlie delta'
+                >>> pprint(list(phrases(s, size=3, trailing=(1, 1))))
+                [(None, None, 'alpha'),
+                 (None, 'alpha', 'bravo'),
+                 ('alpha', 'bravo', 'charlie'),
+                 ('bravo', 'charlie', 'delta'),
+                 ('charlie', 'delta', None),
+                 ('delta', None, None)]
+                >>> pprint(list(phrases(s, size=3, trailing=(None, 2))))
+                [('alpha', 'bravo', 'charlie'),
+                 ('bravo', 'charlie', 'delta'),
+                 ('charlie', 'delta', None)]
+    """
+    if not wordChar in phrases._reCache:
+        phrases._reCache[wordChar] = re.compile(r"\b("+wordChar+r"+)\b")
+    wordRe = phrases._reCache[wordChar]
+
+    buf = [] if trailing[0] is None else [None] * (size-trailing[0])
+
+    for word in wordRe.finditer(sentence):
+        buf.append(word.group(0))
+        if len(buf) > size:
+            buf.pop(0)
+        if len(buf) == size:
+            yield tuple(buf)
+
+    if trailing[1] is not None:
+        for i in xrange(size-trailing[1]):
+            buf.append(None)
+            buf.pop(0)
+            yield tuple(buf)
+
+phrases._reCache = {}
+
 # vim:set ts=4 sw=4 expandtab:
