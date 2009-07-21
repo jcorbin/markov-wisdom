0bea33a8e760e70611dbe9784d9d49ad03643a34
Joshua T Corbin <jcorbin@wunjo.org>
2009-07-20 20:54:56 -0400

    Cleanup word-wrapping generator

--- wisdom.py
+++ wisdom.py
@@ -3,6 +3,28 @@
 import markov
 import random
 
+def wordwrap(source, width=40, sep=' '):
+    """
+    Yields lines of no more than width length built from joining words
+    from the source.
+
+    Yielded lines do not have a trailing newline.
+    """
+    words = (word
+        for chunk in source
+        for word in chunk.strip().split()
+    )
+
+    buf = []
+    for word in words:
+        buf.append(word)
+        line = sep.join(buf)
+        if len(line) > width:
+            yield sep.join(buf[:-1])
+            buf = buf[-1:]
+    if len(buf):
+        yield sep.join(buf)
+
 class wisdom(object):
     paragraphSize = (2, 5)
     sentenceSize = (5, 20)
@@ -18,33 +40,21 @@ class wisdom(object):
             self.paragraphSize[1]
         )
         for i in xrange(count):
-            sentence = self.corpus.sentence(
+            yield self.corpus.sentence(
                 min=self.sentenceSize[0],
                 max=self.sentenceSize[1]
             )
-            for word in sentence.split():
-                yield word
-
-    def formattedLines(self, words):
-        buf = []
-        for word in words:
-            buf.append(word)
-            line = ' '.join(buf)
-            if len(line) > self.lineLength:
-                yield ' '.join(buf[:-1])
-                buf = buf[-1:]
-        if len(buf):
-            yield ' '.join(buf)
 
     def passage(self, paragraphs, formatted=True):
         buf = ''
         for i in xrange(paragraphs):
             if formatted:
-                for line in self.formattedLines(self.sens()):
+                for line in wordwrap(self.sens(), self.lineLength):
                     buf += line+"\n"
                 buf += "\n"
             else:
-                buf += ' '.join(self.sens())+"\n\n"
+                for sen in self.sens():
+                    buf += sen+"\n\n"
         return buf.strip()
 
 w = wisdom('wisdom.txt')
