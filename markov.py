7d19692f5dc49a2bc0d7e8b51055ca04c6bd4d6e
Joshua T Corbin <jcorbin@wunjo.org>
2009-07-21 22:48:51 -0400

    Breakout Parse.phrases.

--- markov.py
+++ markov.py
@@ -2,6 +2,7 @@ import re
 import random
 
 sentenceReCache = {}
+phraseReCache = {}
 
 def fileChunks(f, size=1024):
     """
@@ -44,62 +45,57 @@ def sentences(iterable, endPunc=r"\.;"):
                 buf += chunk
                 break
 
-class Parse(object):
-    _phraseReCache = {}
+def phrases(
+    sentence,
+    wordChar=r"[\w\-']",
+    size=3,
+    trailing=(None, None)
+):
+    """
+    Iterates phrases in a sentence
 
-    @classmethod
-    def phrases(
-        cls,
-        sentence,
-        wordChar=r"[\w\-']",
-        size=3,
-        trailing=(None, None)
-    ):
-        """
-        Iterates phrases in a sentence
+    Arguments
+        wordChar: the character class to use in finding words
+        size: how many words long should each phrase be
+        trailing: how many words to yield with None padding elements
+        at either end of the sentence; for example:
+                >>> s = 'alpha bravo charlie delta'
+                >>> pprint(list(
+                ...     phrases(s, size=3, trailing=(1, 1))
+                ... ))
+                [(None, None, 'alpha'),
+                 (None, 'alpha', 'bravo'),
+                 ('alpha', 'bravo', 'charlie'),
+                 ('bravo', 'charlie', 'delta'),
+                 ('charlie', 'delta', None),
+                 ('delta', None, None)]
+                >>> pprint(list(
+                ...     phrases(s, size=3, trailing=(None, 2))
+                ... ))
+                [('alpha', 'bravo', 'charlie'),
+                 ('bravo', 'charlie', 'delta'),
+                 ('charlie', 'delta', None)]
+    """
+    if not wordChar in phraseReCache:
+        phraseReCache[wordChar] = re.compile(
+            r"\b("+wordChar+r"+)\b"
+        )
+    wordRe = phraseReCache[wordChar]
 
-        Arguments
-            wordChar: the character class to use in finding words
-            size: how many words long should each phrase be
-            trailing: how many words to yield with None padding elements
-            at either end of the sentence; for example:
-                    >>> s = 'alpha bravo charlie delta'
-                    >>> pprint(list(
-                    ...     phrases(s, size=3, trailing=(1, 1))
-                    ... ))
-                    [(None, None, 'alpha'),
-                     (None, 'alpha', 'bravo'),
-                     ('alpha', 'bravo', 'charlie'),
-                     ('bravo', 'charlie', 'delta'),
-                     ('charlie', 'delta', None),
-                     ('delta', None, None)]
-                    >>> pprint(list(
-                    ...     phrases(s, size=3, trailing=(None, 2))
-                    ... ))
-                    [('alpha', 'bravo', 'charlie'),
-                     ('bravo', 'charlie', 'delta'),
-                     ('charlie', 'delta', None)]
-        """
-        if not wordChar in cls._phraseReCache:
-            cls._phraseReCache[wordChar] = re.compile(
-                r"\b("+wordChar+r"+)\b"
-            )
-        wordRe = cls._phraseReCache[wordChar]
+    buf = [] if trailing[0] is None else [None] * (size-trailing[0])
 
-        buf = [] if trailing[0] is None else [None] * (size-trailing[0])
+    for word in wordRe.finditer(sentence):
+        buf.append(word.group(0))
+        if len(buf) > size:
+            buf.pop(0)
+        if len(buf) == size:
+            yield tuple(buf)
 
-        for word in wordRe.finditer(sentence):
-            buf.append(word.group(0))
-            if len(buf) > size:
-                buf.pop(0)
-            if len(buf) == size:
-                yield tuple(buf)
-
-        if trailing[1] is not None:
-            for i in xrange(size-trailing[1]):
-                buf.append(None)
-                buf.pop(0)
-                yield tuple(buf)
+    if trailing[1] is not None:
+        for i in xrange(size-trailing[1]):
+            buf.append(None)
+            buf.pop(0)
+            yield tuple(buf)
 
 class SentenceOverrun(Exception):
     pass
@@ -119,7 +115,7 @@ class Corpus(object):
     def phrases(self, trailing=(None, None)):
         for sentence in sentences(self.source):
             sentence = sentence.lower()
-            for phrase in Parse.phrases(sentence, trailing=trailing):
+            for phrase in phrases(sentence, trailing=trailing):
                 yield phrase
 
     def buildDb(self):
